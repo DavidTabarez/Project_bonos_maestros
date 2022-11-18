02533f0241fab74facd700e657519a85ef33dec4
David Tabarez <davicitotabarez@gmail.com>
2022-11-17 18:54:58 -0600

    Creacion de clase y declaracion de variables a operar

---
+++ src/Bonus_maestro.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bonus_maestro {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		double salario = 0;
+		double bonus = 0; 
+		double salario_minimo = 172.87;
+		double puntos = 0;
+		
+		System.out.println("Ingrese los puntos optenidos: ");
+		
+
+	}
+
+}
